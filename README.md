cee154894c9da6ef766fbbf3943006666cf6a7eb
David <juseok.yun@samsung.com>
2021-03-12 12:55:57 +0900

    update code

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# graphql-flask
+# Developing GraphQL with FastAPI
+
+
+
+## Run
+```   
+$ uvicorn main:app
+```
+
+http://localhost:8000
+
+
+## Test
+http://localhost:8000/graphql
+
+```
+{  
+   hello(name: "Adam")
+}
+```
---
+++ main.py
@@ -0,0 +1,25 @@
+#-*- coding:utf-8 -*-
+
+import graphene
+from fastapi import FastAPI
+from starlette.graphql import GraphQLApp
+from graphene import ObjectType, String, Schema
+
+class Query(ObjectType):
+    hello = String(name=String(default_value="stranger"))
+    goodbye = String()
+
+    def resolve_hello(self, info,name):
+        return "Hello " + name + " !"
+
+    def resolve_goodbye(self, info):
+        return 'See ya!!'
+
+
+app = FastAPI(title='ContactQL', description='GraphQL Contact APIs', version='0.1')
+
+@app.get("/")
+async def root():
+    return {"message": "Contact Applications!"}
+
+app.add_route("/graphql", GraphQLApp(schema=Schema(query=Query)))
